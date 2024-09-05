9c6fd610b9d8b0a2a572938877438450291cd0e0
haoran <thresh847@163.com>
2024-09-06 03:05:28 +0800

    init

---
+++ commitlint.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  ignores: [(commit) => commit.includes("init")],
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [2, "always"],
+    "header-max-length": [2, "always", 100],
+    "subject-empty": [2, "never"],
+    "type-empty": [2, "never"],
+    "subject-case": [2, "never"],
+  },
+  scopes: [
+    "build",
+    "chore",
+    "ci",
+    "docs",
+    "feat",
+    "fix",
+    "perf",
+    "refactor",
+    "revert",
+    "style",
+    "test",
+  ],
+};
